630388c83cc9491191af71dc36faf91d4d74f14e
Ankur <ankur.teksun@gmail.com>
2026-03-24 12:58:56 +0530

    JS DOM Methods and Properties.

--- trial.js
+++ trial.js
@@ -1,1 +1,39 @@
-this is test file...
+// finding HTML element using ID
+// let element = document.getElementById("intro");
+// console.log(element.innerHTML);
+
+// let elementText = document.getElementById("text");
+// console.log(elementText.innerHTML);
+
+//finding HTML element using tag name
+
+// let elementTag = document.getElementsByTagName("p");
+// console.log(elementTag);
+
+// document.getElementById("demo").innerHTML = elementTag[0].innerHTML;
+// console.log(elementTag[0].innerHTML);
+
+// finding HTML element using  class name
+
+// let x = document.getElementsByClassName("intro");
+// console.log(x);
+
+//finding element using querySelector method
+
+// let qs = document.querySelector(".intro");
+// qs.innerHTML = "HEy";
+
+// let qs1 = document.querySelectorAll(".intro");
+// qs1[1].innerHTML = "HEy";
+
+// JS can change the text of html tag
+//add text in demo Id
+
+// document.getElementById("demo").innerHTML = "Ankur";
+
+//Dynamic HTML content
+
+// document.getElementById("demo").innerHTML = "Date : " + Date();
+
+//Changing HTML Style
+// document.getElementById("demo").style.color = "red";
